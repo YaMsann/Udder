6a819babc530ac8c9c9ed7818d92480f1c3ce9a1
Keita Yamada <keita.n.m.e@gmail.com>
2020-11-09 21:52:13 +0100

    Add label

---
+++ Udder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Keita Yamada on 2020/11/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
